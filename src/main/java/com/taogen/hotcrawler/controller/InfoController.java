acbbe816cb7018f63773556da9cfb951b0c9b82e
taogen <taogenjia@gmail.com>
2019-07-21 00:16:41 +0800

    Add swager annotations

--- src/main/java/com/taogen/hotcrawler/controller/InfoController.java
+++ src/main/java/com/taogen/hotcrawler/controller/InfoController.java
@@ -1,5 +1,7 @@
 package com.taogen.hotcrawler.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController("InfoController")
 @RequestMapping("/api/v1")
+@Api(description = "Information API")
 public class InfoController
 {
     public static final String PRODUCES_JSON = "application/json;charset=UTF-8";
@@ -18,14 +21,16 @@ public class InfoController
     }
 
     @RequestMapping(value = "/types", method = RequestMethod.GET, produces = PRODUCES_JSON)
+    @ApiOperation("Get All Type of Information.")
     public String getTypes()
     {
-        return null;
+        return "{ \"ret_code\": 0, \"ret_msg\": \"ok\"}";
     }
 
     @RequestMapping(value = "/type/{id}/infos", method = RequestMethod.GET, produces = PRODUCES_JSON)
+    @ApiOperation("Get All Information of specified type.")
     public String getTypeInfos(@PathVariable(value = "id") String id)
     {
-        return null;
+        return "{ \"ret_code\": 0, \"ret_msg\": \"ok\", \"type\":" + id +"}";
     }
 }
