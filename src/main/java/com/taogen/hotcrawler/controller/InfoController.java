208c9392fc05173fd306548478af28d7dcebfcc2
taogen <taogenjia@gmail.com>
2019-07-20 21:34:22 +0800

    Finished integrate junit4

--- src/main/java/com/taogen/hotcrawler/controller/InfoController.java
+++ src/main/java/com/taogen/hotcrawler/controller/InfoController.java
@@ -4,13 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("InfoController")
 @RequestMapping("/info")
 public class InfoController
 {
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    @RequestMapping(value = "/test", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
     public String test()
     {
-        return "this is a test!";
+        return "{ \"ret_code\": 0, \"ret_msg\": \"ok\"}";
     }
 }
--- src/test/java/com/taogen/hotcrawler/AppTest.java
+++ src/test/java/com/taogen/hotcrawler/AppTest.java
@@ -1,11 +1,60 @@
 package com.taogen.hotcrawler;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.ServletContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 /**
  * Unit test for simple App.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class AppTest 
 {
+    @Autowired
+    private WebApplicationContext wac;
 
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup()
+    {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void verifyContext() {
+        ServletContext servletContext = wac.getServletContext();
+
+        org.junit.Assert.assertNotNull(servletContext);
+        Assert.assertTrue(servletContext instanceof MockServletContext);
+        Assert.assertNotNull(wac.getBean("InfoController"));
+    }
+
+    @Test
+    public void testResponseBody() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(get("/info/test")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.ret_code").value(0))
+                .andExpect(jsonPath("$.ret_msg").value("ok"))
+                .andReturn();
+        Assert.assertEquals("application/json;charset=UTF-8", mvcResult.getResponse().getContentType());
+    }
 }
