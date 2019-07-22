9ea625ec2f8c30aa41cb82d4251beda928f2018d
taogen <taogenjia@gmail.com>
2019-07-22 20:00:49 +0800

    initial commit frontend code

--- src/main/java/com/taogen/hotcrawler/frontend/Controller/IndexController.java
+++ src/main/java/com/taogen/hotcrawler/frontend/Controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.taogen.hotcrawler.frontend.Controller;
+package com.taogen.hotcrawler.frontend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
