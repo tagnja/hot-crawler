7c2944b6b50531175cfe8b579095bb29287d9ea9
taogen <taogenjia@gmail.com>
2019-07-23 00:12:18 +0800

    Finished frontend code

--- src/main/java/com/taogen/hotcrawler/frontend/Controller/IndexController.java
+++ src/main/java/com/taogen/hotcrawler/frontend/Controller/IndexController.java
@@ -1,16 +1,18 @@
 package com.taogen.hotcrawler.frontend.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController
 {
+    Logger log = LoggerFactory.getLogger(IndexController.class);
 
     @GetMapping("/")
-    public String main(Model model) {
-        model.addAttribute("message", "hello");
+    public String toIndexPage()
+    {
         return "index"; //view
     }
 }
