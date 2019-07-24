dff278b9d927b55c8d2405c652ecda750cfca868
taogen <taogenjia@gmail.com>
2019-07-24 13:45:49 +0800

    Little update.

--- src/test/java/com/taogen/hotcrawler/commons/crawler/HotProcessorTest.java
+++ src/test/java/com/taogen/hotcrawler/commons/crawler/HotProcessorTest.java
@@ -2,8 +2,6 @@ package com.taogen.hotcrawler.commons.crawler;
 
 import com.taogen.hotcrawler.api.constant.SiteProperties;
 import com.taogen.hotcrawler.api.service.BaseService;
-import com.taogen.hotcrawler.commons.crawler.impl.V2exHotProcessor;
-import com.taogen.hotcrawler.commons.crawler.impl.ZhihuHotProcessor;
 import com.taogen.hotcrawler.commons.entity.Info;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,13 +9,10 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -31,9 +26,6 @@ public class HotProcessorTest
     @Autowired
     private BaseService baseService;
 
-    @Value("${crawler.task.threadPoolNum}")
-    private int THREAD_POOL_NUM;
-
     @Test
     public void getHotInfoTest()
     {
@@ -43,13 +35,12 @@ public class HotProcessorTest
         {
             for (SiteProperties.SiteInfo site : siteInfos)
             {
-                    HotProcessor hotProcessor = (HotProcessor) baseService.getBean(site.getProcessorName());
-                    List<Info> hotList = hotProcessor.crawlHotList();
-                    Assert.assertNotNull(hotList);
-                    log.info("crawl " + site.getName() + " hot list size: " + hotList.size());
-                    Assert.assertTrue(hotList.size() > 0);
+                HotProcessor hotProcessor = (HotProcessor) baseService.getBean(site.getProcessorName());
+                List<Info> hotList = hotProcessor.crawlHotList();
+                Assert.assertNotNull(hotList);
+                log.info("crawl " + site.getName() + " hot list size: " + hotList.size());
+                Assert.assertTrue(hotList.size() > 0);
             }
         }
-
     }
 }
