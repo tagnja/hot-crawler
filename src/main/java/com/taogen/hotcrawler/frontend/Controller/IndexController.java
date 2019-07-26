e0ed7a38209fb1303632aa0c7aaf5219b7dcf8cc
taogen <taogenjia@gmail.com>
2019-07-26 16:50:16 +0800

    Add model to page

--- src/main/java/com/taogen/hotcrawler/frontend/Controller/IndexController.java
+++ src/main/java/com/taogen/hotcrawler/frontend/Controller/IndexController.java
@@ -2,17 +2,25 @@ package com.taogen.hotcrawler.frontend.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController
 {
     Logger log = LoggerFactory.getLogger(IndexController.class);
 
+    @Value("${domain}")
+    private String domain;
+
     @GetMapping("/")
-    public String toIndexPage()
+    public String toIndexPage(Model model)
     {
+        log.debug("Go to index page: " + domain);
+        model.addAttribute("domain", domain);
         return "index"; //view
     }
 }
