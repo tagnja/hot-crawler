0eddd12229629bb2c438e8714427b9e141e7071b
taogen <taogenjia@gmail.com>
2019-08-01 00:31:49 +0800

    Add Bilibili processor

--- src/main/java/com/taogen/hotcrawler/commons/crawler/impl/BaseHotProcessor.java
+++ src/main/java/com/taogen/hotcrawler/commons/crawler/impl/BaseHotProcessor.java
@@ -48,6 +48,9 @@ public class BaseHotProcessor
 
     private void addBasicHeaders(Connection connection)
     {
+        connection.header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+        connection.header("Accept-Encoding", "gzip, deflate, br");
+        connection.header("Accept-Language", "en-US,en;q=0.5");
         connection.header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0");
     }
 }
---
+++ src/main/java/com/taogen/hotcrawler/commons/crawler/impl/BilibiliHotProcessor.java
@@ -0,0 +1,125 @@
+package com.taogen.hotcrawler.commons.crawler.impl;
+
+import com.taogen.hotcrawler.commons.crawler.HotProcessor;
+import com.taogen.hotcrawler.commons.entity.Info;
+import com.taogen.hotcrawler.commons.util.OsUtils;
+import org.apache.tomcat.util.net.openssl.OpenSSLUtil;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component("BilibiliHotProcessor")
+public class BilibiliHotProcessor implements HotProcessor
+{
+    private static final Logger log = LoggerFactory.getLogger(BilibiliHotProcessor.class);
+    private static final String OS = System.getProperty("os.name").toLowerCase();
+
+    @Autowired
+    private BaseHotProcessor baseHotProcessor;
+
+    @Value("${crawler.chromeDriver.enable}")
+    private Boolean enable;
+
+    @Value("${crawler.chromeDriver.linuxPath}")
+    private String linuxPath;
+
+    @Value("${crawler.chromeDriver.winPath}")
+    private String winPath;
+
+    public static final String DOMAIN = "https://www.bilibili.com";
+    public static final String HOT_PAGE_URL = "https://www.bilibili.com/v/kichiku/guide/#/all/click/0/1/";
+//    public static final String HOT_PAGE_URL = "https://www.bilibili.com/v/kichiku/guide/";
+    public static final String ITEM_KEY = "l-item";
+
+
+    @Override
+    public List<Info> crawlHotList()
+    {
+        List<Info> list = new ArrayList<>();
+        if (! enable)
+        {
+            return list;
+        }
+        String driverPath = null;
+        String osType = OsUtils.getOsType();
+        log.info("os type is {}", osType);
+        if (OsUtils.OS_TYPE_WINDOWS.equals(osType))
+        {
+            driverPath = winPath;
+        }
+        else if (OsUtils.OS_TYPE_LINUX.equals(osType))
+        {
+            driverPath = linuxPath;
+        }
+        else
+        {
+            return list;
+        }
+
+        // doc
+        Document doc = null;
+        try
+        {
+            log.info("driver path: {}", driverPath);
+            System.setProperty("webdriver.chrome.driver", driverPath);
+            WebDriver driver = new ChromeDriver();
+            driver.get(HOT_PAGE_URL);
+            String pageSource = driver.getPageSource();
+            doc = Jsoup.parse(pageSource);
+            driver.close();
+        }
+        catch (RuntimeException e)
+        {
+            log.error("Something error!", e);
+        }
+
+        if (doc == null)
+        {
+            return list;
+        }
+
+        // elements
+        Elements elements = doc.getElementsByClass(ITEM_KEY);
+        log.debug("elements size is {}", elements.size());
+
+        int i = 0;
+        for (Element element : elements)
+        {
+            Element itemElement = null;
+            try
+            {
+                itemElement = element.getElementsByClass("title").get(0);
+            }
+            catch (NullPointerException | IndexOutOfBoundsException e)
+            {
+                log.error("Can't found item element by attribute!", e);
+                continue;
+            }
+            // id
+            String id = String.valueOf(++i);
+
+            // url
+            String infoUrl = itemElement.attr("href").substring(2);
+            infoUrl = "https://" + infoUrl;
+
+            // title
+            String infoTitle = itemElement.html();
+
+            list.add(new Info(id, infoTitle, infoUrl));
+        }
+        log.debug("list size is {}", list.size());
+        return list;
+    }
+}
