d2f3bcfe9b3cc7482394f8d490017870f469e91a
taogen <taogenjia@gmail.com>
2019-08-02 04:51:35 +0800

    initial add sites processor of abroad

---
+++ src/main/java/com/taogen/hotcrawler/commons/crawler/impl/abroad/BBCNewsHotProcessor.java
@@ -0,0 +1,68 @@
+package com.taogen.hotcrawler.commons.crawler.impl.abroad;
+
+import com.taogen.hotcrawler.commons.crawler.HotProcessor;
+import com.taogen.hotcrawler.commons.crawler.impl.BaseHotProcessor;
+import com.taogen.hotcrawler.commons.crawler.impl.technique.DeveloperHotProcessor;
+import com.taogen.hotcrawler.commons.entity.Info;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component("BBCNewsHotProcessor")
+public class BBCNewsHotProcessor implements HotProcessor
+{
+    private static final Logger log = LoggerFactory.getLogger(BBCNewsHotProcessor.class);
+
+    @Autowired
+    private BaseHotProcessor baseHotProcessor;
+
+    public static final String DOMAIN = "https://www.bbc.com";
+    public static final String HOT_PAGE_URL = "https://www.bbc.com/news";
+    public static final String ITEM_KEY = "nw-c-top-stories--standard"; // block: .nw-c-top-stories--standard, title: .gs-c-promo-heading__title,url: .gs-c-promo-heading
+
+    @Override
+    public List<Info> crawlHotList()
+    {
+        List<Info> list = new ArrayList<>();
+
+        // document
+        Document doc = baseHotProcessor.getDoc(HOT_PAGE_URL, null, log);
+        if (doc == null)
+        {
+            return list;
+        }
+
+        // elements
+        Elements elements = doc.getElementsByClass(ITEM_KEY);
+
+        int i = 0;
+        for (Element element : elements)
+        {
+            try
+            {
+                String infoTitle = element.getElementsByClass("gs-c-promo-heading__title").html();
+                StringBuilder infoUrl = new StringBuilder();
+                infoUrl.append(DOMAIN);
+                infoUrl.append(element.attr("href"));
+                String id = String.valueOf(++i);
+                list.add(new Info(id, infoTitle, infoUrl.toString()));
+            }
+            catch(IndexOutOfBoundsException e)
+            {
+                log.error("Can't find attribute!", e);
+            }
+        }
+
+        log.debug("return list size is {}", list.size());
+        // TODO
+//        log.debug("list is {}", list);
+        return list;
+    }
+}
---
+++ src/main/java/com/taogen/hotcrawler/commons/crawler/impl/abroad/YouTubeHotProcessor.java
@@ -0,0 +1,67 @@
+package com.taogen.hotcrawler.commons.crawler.impl.abroad;
+
+import com.taogen.hotcrawler.commons.crawler.HotProcessor;
+import com.taogen.hotcrawler.commons.crawler.impl.BaseHotProcessor;
+import com.taogen.hotcrawler.commons.crawler.impl.technique.DeveloperHotProcessor;
+import com.taogen.hotcrawler.commons.entity.Info;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component("YouTubeHotProcessor")
+public class YouTubeHotProcessor implements HotProcessor
+{
+    private static final Logger log = LoggerFactory.getLogger(YouTubeHotProcessor.class);
+
+    @Autowired
+    private BaseHotProcessor baseHotProcessor;
+
+    public static final String DOMAIN = "https://www.youtube.com";
+    public static final String HOT_PAGE_URL = "https://www.youtube.com/feed/trending";
+    public static final String ITEM_KEY = "title-and-badge";
+
+    @Override
+    public List<Info> crawlHotList()
+    {
+        List<Info> list = new ArrayList<>();
+
+        // document
+        Document doc = baseHotProcessor.getDoc(HOT_PAGE_URL, null, log);
+        if (doc == null)
+        {
+            return list;
+        }
+
+        // elements
+        Elements elements = doc.getElementsByClass(ITEM_KEY);
+
+        int i = 0;
+        for (Element element : elements)
+        {
+            try
+            {
+                Element item = element.getElementsByTag("a").get(0);
+                String infoTitle = item.html();
+                StringBuilder infoUrl = new StringBuilder();
+                infoUrl.append(DOMAIN);
+                infoUrl.append(item.attr("href"));
+                String id = String.valueOf(++i);
+                list.add(new Info(id, infoTitle, infoUrl.toString()));
+            }
+            catch(IndexOutOfBoundsException e)
+            {
+                log.error("Can't find attribute!", e);
+            }
+        }
+
+        log.debug("return list size is {}", list.size());
+        return list;
+    }
+}
