319a60d38de27705b817518095c2ba07f4c1fe79
taogen <taogenjia@gmail.com>
2019-08-05 13:38:18 +0800

    Add news sites

---
+++ src/main/java/com/taogen/hotcrawler/commons/crawler/impl/news/GeekParkHotProcessor.java
@@ -0,0 +1,72 @@
+package com.taogen.hotcrawler.commons.crawler.impl.news;
+
+import com.taogen.hotcrawler.commons.crawler.HotProcessor;
+import com.taogen.hotcrawler.commons.crawler.impl.BaseHotProcessor;
+import com.taogen.hotcrawler.commons.crawler.impl.technique.DeveloperHotProcessor;
+import com.taogen.hotcrawler.commons.entity.Info;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component("GeekParkHotProcessor")
+public class GeekParkHotProcessor implements HotProcessor
+{
+    private static final Logger log = LoggerFactory.getLogger(GeekParkHotProcessor.class);
+
+    @Autowired
+    private BaseHotProcessor baseHotProcessor;
+
+    public static final String DOMAIN = "https://www.geekpark.net";
+    public static final String HOT_PAGE_URL = "https://www.geekpark.net";
+    public static final String ITEM_KEY = "article-item";
+
+    @Override
+    public List<Info> crawlHotList()
+    {
+        List<Info> list = new ArrayList<>();
+
+        // document
+        Document doc = baseHotProcessor.getDoc(HOT_PAGE_URL, null, log);
+        if (doc == null)
+        {
+            return list;
+        }
+
+        // elements
+        Elements elements = doc.getElementsByClass(ITEM_KEY);
+
+        int i = 0;
+        for (Element element : elements)
+        {
+            try
+            {
+                Element item = element.getElementsByClass("img-cover-wrap").get(0);
+                String infoTitle = item.attr("data-event-label");
+                int index = infoTitle.indexOf("/news");
+                if (index != -1)
+                {
+                    infoTitle = infoTitle.substring(0, index);
+                }
+                StringBuilder infoUrl = new StringBuilder();
+                infoUrl.append(DOMAIN);
+                infoUrl.append(item.attr("href"));
+                String id = String.valueOf(++i);
+                list.add(new Info(id, infoTitle, infoUrl.toString()));
+            }
+            catch(IndexOutOfBoundsException e)
+            {
+                log.error("Can't find attribute!", e);
+            }
+        }
+
+        log.debug("return list size is {}", list.size());
+        return list;
+    }
+}
---
+++ src/main/java/com/taogen/hotcrawler/commons/crawler/impl/news/HuxiuHotProcessor.java
@@ -0,0 +1,67 @@
+package com.taogen.hotcrawler.commons.crawler.impl.news;
+
+import com.taogen.hotcrawler.commons.crawler.HotProcessor;
+import com.taogen.hotcrawler.commons.crawler.impl.BaseHotProcessor;
+import com.taogen.hotcrawler.commons.crawler.impl.technique.DeveloperHotProcessor;
+import com.taogen.hotcrawler.commons.entity.Info;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component("HuxiuHotProcessor")
+public class HuxiuHotProcessor implements HotProcessor
+{
+    private static final Logger log = LoggerFactory.getLogger(HuxiuHotProcessor.class);
+
+    @Autowired
+    private BaseHotProcessor baseHotProcessor;
+
+    public static final String DOMAIN = "https://www.huxiu.com";
+    public static final String HOT_PAGE_URL = "https://www.huxiu.com/article";
+    public static final String ITEM_KEY = "article-items";
+
+    @Override
+    public List<Info> crawlHotList()
+    {
+        List<Info> list = new ArrayList<>();
+
+        // document
+        Document doc = baseHotProcessor.getDoc(HOT_PAGE_URL, null, log);
+        if (doc == null)
+        {
+            return list;
+        }
+
+        // elements
+        Elements elements = doc.getElementsByClass(ITEM_KEY);
+
+        int i = 0;
+        for (Element element : elements)
+        {
+            try
+            {
+                Element item = element.getElementsByClass("article-item__content").get(0);
+                String infoTitle = item.getElementsByClass("article-item__content__title").get(0).html();
+                StringBuilder infoUrl = new StringBuilder();
+                infoUrl.append(DOMAIN);
+                infoUrl.append(item.getElementsByTag("a").get(0).attr("href"));
+                String id = String.valueOf(++i);
+                list.add(new Info(id, infoTitle, infoUrl.toString()));
+            }
+            catch(IndexOutOfBoundsException e)
+            {
+                log.error("Can't find attribute!", e);
+            }
+        }
+
+        log.debug("return list size is {}", list.size());
+        return list;
+    }
+}
