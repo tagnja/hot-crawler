bd44394752bed4ed26a05d23beb82b10378dda07
taogen <taogenjia@gmail.com>
2019-10-17 23:40:57 +0800

    Update redis config

--- src/main/java/com/taogen/hotcrawler/commons/config/RedisConfig.java
+++ src/main/java/com/taogen/hotcrawler/commons/config/RedisConfig.java
@@ -1,20 +1,39 @@
 package com.taogen.hotcrawler.commons.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
 @EnableCaching
+@EnableRedisRepositories
 public class RedisConfig extends CachingConfigurerSupport
 {
-
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+    @Primary
+    public RedisProperties redisProperties() {
+        return new RedisProperties();
+    }
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory()
+    {
+        RedisProperties properties = redisProperties();
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(properties.getHost());
+        configuration.setPort(properties.getPort());
+        configuration.setPassword(properties.getPassword());
+        configuration.setDatabase(properties.getDatabase());
+        JedisConnectionFactory factory = new JedisConnectionFactory(configuration);
+        return factory;
     }
 
     @Bean(value = "redisTemplate")
@@ -23,6 +42,4 @@ public class RedisConfig extends CachingConfigurerSupport
         template.setConnectionFactory(jedisConnectionFactory());
         return template;
     }
-
-
 }
