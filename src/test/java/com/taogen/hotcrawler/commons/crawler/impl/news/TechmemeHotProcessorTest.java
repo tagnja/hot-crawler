30682815e7fb94d32e8f0171e9097c575359afe4
taogen <taogenjia@gmail.com>
2020-01-21 15:51:25 +0800

    Update file path

--- src/test/java/com/taogen/hotcrawler/commons/crawler/impl/news/TechmemeHotProcessorTest.java
+++ src/test/java/com/taogen/hotcrawler/commons/crawler/impl/news/TechmemeHotProcessorTest.java
@@ -1,11 +1,10 @@
 package com.taogen.hotcrawler.commons.crawler.impl.news;
 
 import com.taogen.hotcrawler.commons.crawler.HotProcessorTest;
+import com.taogen.hotcrawler.commons.crawler.impl.abroad.TechmemeHotProcessor;
 import org.junit.Test;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import static org.junit.Assert.*;
 
 public class TechmemeHotProcessorTest extends HotProcessorTest {
 
